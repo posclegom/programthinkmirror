c6c7e5eca19a8ea631ad9d0f03597982b8db7d9d
root <root@localhost.localdomain>
2017-05-01 18:15:29 +0300

    93rd commit

--- master1.sh
+++ master1.sh
@@ -4,7 +4,9 @@ i=0
 while read p; do
  echo $p
  echo $i
- a="sed -i -e 's/https:\/\/program-think.blogspot.com/..\/../g'"
- eval "$a .$p"
+ a="sed -i -e s/https:\/\/program-think.blogspot.com/..\/../g"
+ b="$a .$p"
+ echo $b
+ $b
  ((i++))
 done < mlist 
--- src/pt18/master7.sh
+++ src/pt18/master7.sh
@@ -8,30 +8,30 @@ k="forum3/"
 while read p; do
  echo $p
  echo $i
- a="h1><a href=\"..\/..\/forum3\/"
- b="a.html\">View page A of comments for this blog article.<\/a><\/h1>\n<h1>"
- c="sed -i -e '0,/h1>/{s/h1>/$a$i$b/}' .$p"
+ a="h1><a\x20href=\"..\/..\/forum3\/"
+ b="a.html\">View\x20page\x20A\x20of\x20comments\x20for\x20this\x20blog\x20article.<\/a><\/h1>\n<h1>"
+ c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
- eval $c
+ $c
  if [ -e $k$i$j ]
  then
   echo EXISTS!
   echo $k$i$j
- a="h1><a href=\"..\/..\/forum3\/"
- b="b.html\">View page B of comments for this blog article.<\/a><\/h1>\n<h1>"
- c="sed -i -e '0,/h1>/{s/h1>/$a$i$b/}' .$p"
+ a="h1><a\x20href=\"..\/..\/forum3\/"
+ b="b.html\">View\x20page\x20B\x20of\x20comments\x20for\x20this\x20blog\x20article.<\/a><\/h1>\n<h1>"
+ c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
- eval $c
+ $c
  fi
  if [ -e $k$i$l ]
  then
   echo EXISTS C!
   echo $k$i$l
- a="h1><a href=\"..\/..\/forum3\/"
- b="c.html\">View page C of comments for this blog article.<\/a><\/h1>\n<h1>"
- c="sed -i -e '0,/h1>/{s/h1>/$a$i$b/}' .$p"
+ a="h1><a\x20href=\"..\/..\/forum3\/"
+ b="c.html\">View\x20page\x20C\x20of\x20comments\x20for\x20this\x20blog\x20article.<\/a><\/h1>\n<h1>"
+ c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
- eval $c
+ $c
  fi
  ((i++))
 done < mlist 
