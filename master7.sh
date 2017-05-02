468afa53ec8da8720e369c1f1d3f43614abac67c
root <root@localhost.localdomain>
2017-05-02 03:52:09 +0300

    94th commit

--- master7.sh
+++ master7.sh
@@ -9,7 +9,7 @@ while read p; do
  echo $p
  echo $i
  a="h1><a\x20href=\"..\/..\/forum3\/"
- b="a.html\">View\x20page\x20A\x20of\x20comments\x20for\x20this\x20blog\x20article.<\/a><\/h1>\n<h1>"
+ b="a.html\">浏览本博文的相关论坛甲版<\/a><\/h1>\n<h1>"
  c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
  $c
@@ -18,7 +18,7 @@ while read p; do
   echo EXISTS!
   echo $k$i$j
  a="h1><a\x20href=\"..\/..\/forum3\/"
- b="b.html\">View\x20page\x20B\x20of\x20comments\x20for\x20this\x20blog\x20article.<\/a><\/h1>\n<h1>"
+ b="b.html\">浏览本博文的相关论坛乙版<\/a><\/h1>\n<h1>"
  c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
  $c
@@ -28,7 +28,7 @@ while read p; do
   echo EXISTS C!
   echo $k$i$l
  a="h1><a\x20href=\"..\/..\/forum3\/"
- b="c.html\">View\x20page\x20C\x20of\x20comments\x20for\x20this\x20blog\x20article.<\/a><\/h1>\n<h1>"
+ b="c.html\">浏览本博文的相关论坛丙版<\/a><\/h1>\n<h1>"
  c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
  $c
