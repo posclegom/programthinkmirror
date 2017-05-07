64af170a9849b1bd14629199ca02a53ecab7442e
root <root@localhost.localdomain>
2017-05-07 05:22:32 +0300

    95th commit

--- master1.sh
+++ master1.sh
@@ -4,7 +4,7 @@ i=0
 while read p; do
  echo $p
  echo $i
- a="sed -i -e s/https:\/\/program-think.blogspot.com/..\/../g"
+ a="sed -i -e s/https:\/\/program-think.blogspot.com/..\/../"
  b="$a .$p"
  echo $b
  $b
