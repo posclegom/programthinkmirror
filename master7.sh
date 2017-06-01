3c02505301386dde7cc4f516ba07a4b6d5c24377
root <root@localhost.localdomain>
2017-06-01 14:02:00 +0300

    99th commit

--- master7.sh
+++ master7.sh
@@ -3,12 +3,12 @@
 i=0
 j=b.html
 l=c.html
-k="forum3/"
+k="forum4/"
 
 while read p; do
  echo $p
  echo $i
- a="h1><a\x20href=\"..\/..\/forum3\/"
+ a="h1><a\x20href=\"..\/..\/forum4\/"
  b="a.html\">浏览本博文的相关论坛甲版<\/a><\/h1>\n<h1>"
  c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
@@ -17,7 +17,7 @@ while read p; do
  then
   echo EXISTS!
   echo $k$i$j
- a="h1><a\x20href=\"..\/..\/forum3\/"
+ a="h1><a\x20href=\"..\/..\/forum4\/"
  b="b.html\">浏览本博文的相关论坛乙版<\/a><\/h1>\n<h1>"
  c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
@@ -27,7 +27,7 @@ while read p; do
  then
   echo EXISTS C!
   echo $k$i$l
- a="h1><a\x20href=\"..\/..\/forum3\/"
+ a="h1><a\x20href=\"..\/..\/forum4\/"
  b="c.html\">浏览本博文的相关论坛丙版<\/a><\/h1>\n<h1>"
  c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
