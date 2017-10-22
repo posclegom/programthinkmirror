ebb817c8bba95ed61b1b946d6af8563ae66caa8c
root <root@localhost.localdomain>
2017-10-22 20:11:55 +0300

    130th commit

--- forum4/sensor.sh
+++ forum4/sensor.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+i=0
+l=c.xml
+
 while read p; do
  read q <&3
  if [ "$p" -gt 1000 ] || [ "$q" -gt 1000 ]
  then
-  echo "$p $q $(( p - q ))"
+  if [ -e $i$l ]
+  then
+  echo -e "$p $q $(( p - q )) \t $i $j EXISTS!"
+  else
+  echo -e "$p $q $(( p - q )) \t $i $j NULL!"
+  fi
  fi
+ ((i++))
 done <point 3<newww
